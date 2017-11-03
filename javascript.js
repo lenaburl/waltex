5b2f04e1331eec07b0d46a780568280feb9f3b88
Elena <lenaburl@mail.ru>
2017-11-03 23:36:06 +0700

    Make adaptive

--- javascript.js
+++ javascript.js
@@ -14,6 +14,13 @@ function RegistrationVisible () {
     popup_registration.addClass("registration-pop-up--visible");
     $(".registration-form__email").focus();
   });
+
+  $(".main-capabilities__try-btn").click(function (event){
+    var popup_registration = $(".registration-pop-up");
+    event.preventDefault();
+    popup_registration.addClass("registration-pop-up--visible");
+    $(".registration-form__email").focus();
+  });
 }
 
 function LoginInvisible () {
@@ -32,9 +39,70 @@ function RegistrationInvisible () {
   });
 }
 
+function Carousel () {
+  $(".features-list__circle").click(function (){
+    var btn_id = $(this).attr( "id");
+    if (btn_id == "left") {
+      $(".features-list").css("transform", "translate(0)");
+      $(".features-list__left").addClass("features-list__circle--inactive");
+      $(".features-list__right").addClass("features-list__circle--active");
+      $(".features-list__right").removeClass("features-list__circle--inactive");
+      $(".features-list__left").removeClass("features-list__circle--active");
+    }
+    else if (btn_id == "right") {
+      $(".features-list").css("transform", "translate(-615px)");
+      $(".features-list__left").addClass("features-list__circle--active");
+      $(".features-list__right").addClass("features-list__circle--inactive");
+      $(".features-list__right").removeClass("features-list__circle--active");
+      $(".features-list__left").removeClass("features-list__circle--inactive");
+    }
+  });
+}
+
+function ChangeText () {
+  if ($(window).width() < 640) {
+    $(".btn__enter").text("");
+    $(".main-capabilities__link").removeClass("main-capabilities__link--hidden");
+    $(".main-capabilities__try-btn").removeClass("main-capabilities__try-btn--hidden");
+  }
+  else {
+    $(".btn__enter").text("Войти");
+    $(".main-capabilities__link").addClass("main-capabilities__link--hidden");
+    $(".main-capabilities__try-btn").addClass("main-capabilities__try-btn--hidden");
+  }
+}
+
+
+function MinimizeTable () {
+  if ( $(window).width() < 640 )
+  {
+      $(".price").each(function() {
+        var $this = $(this);
+        var newrows = [];
+        $this.find("tr").each(function(){
+            var i = 0;
+            $(this).find("td").each(function(){
+                i++;
+                if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+                newrows[i].append($(this));
+            });
+        });
+        $this.find("tr").remove();
+        $.each(newrows, function(){
+            $this.append(this);
+        });
+      });
+  }
+}
+
+
+
 $(document).ready(function() {
   LoginVisible();
   RegistrationVisible();
   LoginInvisible();
   RegistrationInvisible();
+  MinimizeTable();
+  Carousel();
+  ChangeText();
 });
